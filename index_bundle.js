8e56b8caffa025202476270771729aede011a152
bowei <bowei.han100@gmail.com>
2016-08-21 12:08:00 -0400

    styles

--- index_bundle.js
+++ index_bundle.js
@@ -27199,12 +27199,26 @@
 	  },
 	  container: {
 	    'backgroundColor': 'white',
-	    'margin': 'auto',
+	    'margin': '24px auto',
+	    'padding': '16px 0',
+	    'width': '60%',
 	    'border': '0.3px solid lightgray'
 	  },
-	  username: {},
-	  message: {},
-	  date: {}
+	  username: {
+	    'font-size': '20px',
+	    'color': 'rgba(0,0,0,0.7)',
+	    'margin-bottom': '8px'
+	  },
+	  message: {
+	    'font-size': '16px',
+	    'color': 'rgba(0,0,0,0.7)',
+	    'margin-bottom': '8px'
+
+	  },
+	  date: {
+	    'font-size': '14px',
+	    'color': 'rgba(0,0,0,0.5)'
+	  }
 	};
 
 	module.exports = styles;
